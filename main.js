d617ee3b47b5bc281f728b6ed0d1f2f1825e4d35
Ian <iwade0704@gmail.com>
2023-06-01 10:54:56 -0500

    railway commit

--- main.js
+++ main.js
@@ -4,11 +4,17 @@ new fullpage('#fullpage', {
   scrollHorizontally: true,
   licenseKey: 'gplv3-license',
   anchors:['landing', 'about', 'projects', 'contact'],
+  scrollBar: false,
+  scrollHorizontally: false,
+  css3: true,
+  scrollingSpeed: 800,
+  easingcss3: 'cubic-bezier(0.33, 1, 0.2, 1)',
+  // Accessibility
+  keyboardScrolling: true,
+  animateAnchor: true,
+  recordHistory: true,
   onLeave: function(origin, destination, direction, trigger){
-    console.log("scroll");
-
-    console.log(destination);
-
+    
     if(destination.anchor == "landing") {
       document.querySelector('nav').style.opacity = '0';
       document.querySelector('header').style.opacity = '0';
@@ -17,24 +23,43 @@ new fullpage('#fullpage', {
       document.querySelector('nav').style.opacity = '1';
       document.querySelector('header').style.opacity = '1';
     }
+
+    let nav_highlight = "nav--" + destination.anchor;
+    let nav_unhighlight = "nav--" + origin.anchor;
+
+    document.getElementById(nav_highlight).style.opacity = '100%';
+    document.getElementById(nav_unhighlight).style.opacity = '25%';
   },
   credits: { enabled: false, label: 'Made with fullPage.js', position: 'right'},
 });
 
-document.querySelector('nav').style.opacity = '0';
-document.querySelector('header').style.opacity = '0';
+const projects = document.querySelectorAll('.projects--item-full');
 
-const items = document.querySelectorAll('.projects--item-full');
+projects.forEach(project => {
+  const project_contents = project.querySelector('.projects--item-contents');
 
-items.forEach(item => {
-  item.addEventListener('click', () => {
-    const item_contents = item.querySelector('.projects--item-contents');
-    item_contents.style.height = (item_contents.style.height == '0px') ? '100%' : '0px';
-    /*item_contents.style.display = (item_contents.style.display == 'none') ? 'block' : 'none';*/
+  project.querySelectorAll('.projects--item-thumbnail').forEach(thumbnail => {
+    thumbnail.addEventListener('click', () => {
+      toggle_project(project_contents);
+    });
+  });
 
-    item.style.height = (item.style.height === '250px') ? '100%' : '250px';
+  project.querySelectorAll('.projects--closer').forEach(closer => {
+    closer.addEventListener('click', () => {
+      toggle_project(project_contents);
+    });
   });
 });
+
+/*closers.forEach(item => {
+  item.addEventListener('click', toggle_project(this))
+});*/
+
+function toggle_project(item_contents) {
+  
+    item_contents.style.height = (item_contents.style.height == '0px') ? '100%' : '0px';
+    item_contents.style.display = (item_contents.style.display == 'none') ? 'block' : 'none';
+}
 
 function navigateToSection(sectionId) {
   const section = document.getElementById(sectionId);
@@ -52,3 +77,8 @@ function navigateToSection(sectionId) {
     });
   }
 }
+
+$(document).ready(function () {
+  $(".projects--filter").scrollspy({ offset: -25 });
+});
+
