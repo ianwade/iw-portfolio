c1514b11ba6c3075e05d706478fc715f118614cf
Ian <iwade0704@gmail.com>
2023-06-07 18:44:49 -0500

    content + fixes

--- src/js/3dlanding.js
+++ src/js/3dlanding.js
@@ -11,8 +11,13 @@ const renderer = new THREE.WebGLRenderer({ canvas, antialias: true, alpha: true 
 renderer.setSize( 550, 300 );
 renderer.outputColorSpace = THREE.SRGBColorSpace;
 
-const light = new THREE.PointLight( 0xffffff, 5, 100 );
-light.position.set( 0, 0, 100 );
+const light_direct = new THREE.AmbientLight( 0xfc7de3, .7, 100 );
+
+const light_side = new THREE.PointLight( 0xfffce0, 4, 100 );
+light_side.position.set( 0, 0, 100 );
+
+const light_side2 = new THREE.PointLight( 0xb27dfc, 3, 100 );
+light_side2.position.set( -10, 0, 100 );
 
 /*const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const cube = new THREE.Mesh( geometry, material );
@@ -28,7 +33,9 @@ loader.load( '../src/assets/ianlogo2.glb', ( gltf ) => {
     gltf.scene.position.set(0, 0, 47);
 
 	scene.add( gltf.scene );
-    scene.add( light );
+    scene.add( light_direct );
+    scene.add( light_side );
+    //scene.add( light_side2 );
 
 }, undefined, function ( error ) {
 
@@ -59,28 +66,19 @@ function animate() {
     const bg_mover = document.getElementById('landing--bg');
 
     onmousemove = function(e) {
-
-        if(e.clientX == 0 || e.clientY == 0)
-        {
-
-        }
-
-        //Y axis movement
-        if(e.clientY > screen_height_min && e.clientY < screen_height_max)
+        //up-down movement
+        if(e.clientY >= screen_height_min && e.clientY <= screen_height_max)
             logo_object.scene.rotation.x = (e.clientY - originY) / rotate_strength;
 
-        //X axis momement
-        if(e.clientX > screen_width_min && e.clientX < screen_width_max)
+        //left-right axis momement
+        if(e.clientX >= screen_width_min && e.clientX <= screen_width_max)
             logo_object.scene.rotation.y = (e.clientX - originX) / rotate_strength;
 
         //extrusion
-        logo_object.scene.scale.z = Math.abs(e.clientX - originX) / scale_strength;
+        logo_object.scene.scale.z = Math.abs(e.clientX - originX) / scale_strength + 1;
 
         bg_mover.style.backgroundPositionX = -1 * (e.clientX / 20) + "px";
         bg_mover.style.backgroundPositionY = -1 * (e.clientY / 20) + "px";
-
-        console.log(bg_mover.style.backgroundPositionX);
-        console.log(bg_mover.style.backgroundPositionY);
     
     }
 
